0ed1a1a6b6cc8e8b514af5579b08c470fdb8e7b3
suntaehun <sth7862@naver.com>
2022-10-11 17:22:57 +0900

    dcl 연습2

--- sql 연습/dcl-practices.sql
+++ sql 연습/dcl-practices.sql
@@ -4,4 +4,14 @@ create database webdb;
 -- 데이터베이스 생성확인
 show databases;
 
+-- db 사용자 삭제
+drop user 'webdb'@'localhost';
+
 -- db 사용자 생성
+create user 'webdb'@'localhost' identified by 'webdb';
+
+-- 권한부여
+grant all privileges on webdb.* to 'webdb'@'localhost';
+
+-- 권한정보 재로딩
+flush privileges;
